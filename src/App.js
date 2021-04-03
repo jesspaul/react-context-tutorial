32c5fa29f734c9773ecf21bd93d2dbbfb2492cfd
Jessica Paul <paul.jessica95@gmail.com>
2021-04-03 15:50:04 -0400

    add Context and wrap components with provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import BookList from "./components/BookList/BookList";
 import Navbar from "./components/Navbar/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
