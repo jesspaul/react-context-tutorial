400c51c4097dec41d22555f1c6a5eba7fa03d1e8
Jessica Paul <paul.jessica95@gmail.com>
2021-04-03 15:23:18 -0400

    initial project setup

---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+const BookList = props => (
+    <div className="book-list">
+        <ul>
+            <li>Gone Girl</li>
+            <li>Big Little Lies</li>
+            <li>Sharp Objects</li>
+        </ul>
+    </div>
+);
+
+export default BookList;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = props => (
+    <nav>
+        <h1>Context App</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+export default Navbar;
