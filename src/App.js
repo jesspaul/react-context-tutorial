e576b8394664e619e92332476ae241a27353d218
Jessica Paul <paul.jessica95@gmail.com>
2021-04-03 16:47:11 -0400

    consume multiple contexts in one component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import BookList from "./components/BookList/BookList";
 import Navbar from "./components/Navbar/Navbar";
 import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
