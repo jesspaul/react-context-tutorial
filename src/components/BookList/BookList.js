e2e9573af748074acbf7590f4d1bd213ec1412fb
Jessica Paul <paul.jessica95@gmail.com>
2021-04-03 16:01:59 -0400

    apply theme context to components

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,11 +1,21 @@
-const BookList = props => (
-    <div className="book-list">
-        <ul>
-            <li>Gone Girl</li>
-            <li>Big Little Lies</li>
-            <li>Sharp Objects</li>
-        </ul>
-    </div>
-);
+import React, { Component } from 'react';
+import { ThemeContext } from "../../contexts/ThemeContext";
 
+class BookList extends Component {
+    static contextType = ThemeContext;
+    render() { 
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+        return ( 
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+                <ul>
+                    <li style={{ background: theme.ui }}>Gone Girl</li>
+                    <li style={{ background: theme.ui }}>Big Little Lies</li>
+                    <li style={{ background: theme.ui }}>Sharp Objects</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
 export default BookList;
