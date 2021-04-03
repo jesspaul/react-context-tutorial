4da45d00799409f66c33fcd5c34d4efb6b9c700b
Jessica Paul <paul.jessica95@gmail.com>
2021-04-03 18:27:46 -0400

    refactor to functional component using useContext hook

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from "../../contexts/ThemeContext";
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return ( 
-            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>Gone Girl</li>
-                    <li style={{ background: theme.ui }}>Big Little Lies</li>
-                    <li style={{ background: theme.ui }}>Sharp Objects</li>
-                </ul>
-            </div>
-         );
-    }
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+        <ul>
+            <li style={{ background: theme.ui }}>Gone Girl</li>
+            <li style={{ background: theme.ui }}>Big Little Lies</li>
+            <li style={{ background: theme.ui }}>Sharp Objects</li>
+        </ul>
+    </div>
+    );
 }
  
 export default BookList;
